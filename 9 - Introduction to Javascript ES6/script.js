08a6a90c4d4c0d5aad67bb4dc10108be36860bf7
Ayan Masood <ayan.m.dev@gmail.com>
2023-04-04 06:47:02 +0530

    Update script.js

--- 9 - Introduction to Javascript ES6/script.js
+++ 9 - Introduction to Javascript ES6/script.js
@@ -3,6 +3,8 @@ function noOfDaysLeft (age) {
     var days = yearsremaing * 365;
     var weeks = yearsremaing * 52;
     var months = yearsremaing * 12;
+
+    console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
 }
 
 noOfDaysLeft(25);
