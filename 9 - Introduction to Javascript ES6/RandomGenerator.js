1d5419edb547f070e08b2441b5d5a83f1af9f476
Ayan Masood <ayan.m.dev@gmail.com>
2023-04-11 16:57:27 +0530

    Random() with conditionals

--- 9 - Introduction to Javascript ES6/RandomGenerator.js
+++ 9 - Introduction to Javascript ES6/RandomGenerator.js
@@ -1,5 +1,17 @@
-var firstName = prompt("Enter your name");
-var secondName = prompt("Enter your partner's name");
-var loveScore = Math.random() * 100;
-loveScore = Math.floor(loveScore) + 1; // 1-100
-alert(firstName + " and " + secondName + " love score is " + loveScore + "%");
+// WITHOUT USING CONDITIONALS
+// var firstName = prompt("Enter your name");
+// var secondName = prompt("Enter your partner's name");
+// var loveScore = Math.random() * 100;
+// loveScore = Math.floor(loveScore) + 1; // 1-100
+// alert(firstName + " and " + secondName + " love score is " + loveScore + "%");
+
+// USING CONDITIONALS
+    var firstName = prompt("Enter your name");
+    var secondName = prompt("Enter your partner's name");
+    var loveScore = Math.random() * 100;
+        loveScore = Math.floor(loveScore) + 1; // 1-100
+    if (loveScore > 70) {
+        alert(firstName + " and " + secondName + " love score is " + loveScore + "%" + " You love each other like Kanye loves Kanye.");
+    } else {
+        alert(firstName + " and " + secondName + " love score is " + loveScore + "%" + " You go together like coke and mentos.");
+    }
