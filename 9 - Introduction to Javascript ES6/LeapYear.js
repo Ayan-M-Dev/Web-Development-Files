65320fff8e9d4f2d79c511cce96facbf5085a48a
Ayan Masood <ayan.m.dev@gmail.com>
2023-04-16 21:15:41 +0530

    Leap year check

---
+++ 9 - Introduction to Javascript ES6/LeapYear.js
@@ -0,0 +1,20 @@
+function checkLeapYEar(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return "It is a leap year"
+            }
+        }   else {
+            return "It is not a leap year"
+            }
+        else {
+            return "It is a leap year"
+        }
+    }
+    else {
+        return "It is not a leap year"
+    }
+}
+year = 2000
+console.log(checkLeapYEar(year))
+
