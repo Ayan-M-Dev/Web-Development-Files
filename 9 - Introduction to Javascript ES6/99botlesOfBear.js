4a9bba39ef510f75235c6445864f819f56f7be44
Ayan Masood <ayan.m.dev@gmail.com>
2023-04-19 16:07:20 +0530

    Update 99 cahllenge

--- 9 - Introduction to Javascript ES6/99botlesOfBear.js
+++ 9 - Introduction to Javascript ES6/99botlesOfBear.js
@@ -1,9 +1,9 @@
-i = 99;
+numberOfBottles = 99;
 for (i >=100 ; i >= 0; i--) {
     if (i > 1) {
-        console.log(i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down and pass it around, " + (i-1) + " bottles of beer on the wall.");
+        console.log(numberOfBottles + " bottles of beer on the wall, " + numberOfBottles + " bottles of beer. Take one down and pass it around, " + (numberOfBottles-1) + " bottles of beer on the wall.");
     } else if (i === 1) {
-        console.log(i + " bottle of beer on the wall, " + i + " bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.");
+        console.log(numberOfBottles + " bottle of beer on the wall, " + numberOfBottles + " bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.");
     } else {
         console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
     }
