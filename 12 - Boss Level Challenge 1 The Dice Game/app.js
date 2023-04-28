488a1eeabada65a87b3d9f7e91c9332281df6e5a
Ayan Masood <ayan.m.dev@gmail.com>
2023-04-28 22:58:04 +0530

    RandomNumber

---
+++ 12 - Boss Level Challenge 1 The Dice Game/app.js
@@ -0,0 +1,3 @@
+randomNUmber1 = Math.floor(Math.random() * 6) + 1; // 1-6
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNUmber1 + ".png");
+setAttribute("src", "images/dice" + randomNUmber1 + ".png");
