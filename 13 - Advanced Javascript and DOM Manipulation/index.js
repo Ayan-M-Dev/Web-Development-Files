060916ce86d0c1e96e5bc88ebb0c5729dfaa25a4
Ayan Masood <ayan.m.dev@gmail.com>
2023-04-30 20:10:31 +0530

    Adding event listeners
    With the class name of ".drum"

---
+++ 13 - Advanced Javascript and DOM Manipulation/index.js
@@ -0,0 +1,7 @@
+//Adding event listeners to all  the buttons with the class name 'drum' and putting it through a for loop so that it goes every button with class 'drum'   
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {  
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClicked);
+    function handleClicked() {
+        alert("I got clicked");
+    }
+}    
