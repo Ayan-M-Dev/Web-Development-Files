e0e1ae0b06d1eaed5c59d7cc04f98d387e8b31dd
Ayan Masood <ayan.m.dev@gmail.com>
2023-05-05 22:05:58 +0530

    Switch Cases

--- 13 - Advanced Javascript and DOM Manipulation/index.js
+++ 13 - Advanced Javascript and DOM Manipulation/index.js
@@ -5,10 +5,15 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function() { 
+
+        switch (expression) {
+            case expression:
+        }
         
-        var audio = new Audio ("sounds/tom-1.mp3");
-        audio.play();
-        
+            break;
+            default;
+
+
     });
     
 }
