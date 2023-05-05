d3c2d7e1ead92ff3f4d7061a4736f605c995fc2a
Ayan Masood <ayan.m.dev@gmail.com>
2023-05-05 21:08:47 +0530

    Adding audio to the buttons
    addEventListener

--- 13 - Advanced Javascript and DOM Manipulation/index.js
+++ 13 - Advanced Javascript and DOM Manipulation/index.js
@@ -1,8 +1,14 @@
 //Adding event listeners to all  the buttons with the class name 'drum' and putting it through a for loop so that it goes every button with class 'drum'   
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {  
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClicked);
-    function handleClicked() {
-        alert("I got clicked");
-    }
-}    
 
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() { 
+        
+        var audio = new Audio ("sounds/tom-1.mp3");
+        audio.play();
+        
+    });
+    
+}
