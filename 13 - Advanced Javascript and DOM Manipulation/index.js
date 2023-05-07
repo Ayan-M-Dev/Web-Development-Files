fd39a9bfa2e83f990e6bbf7d79fce2763f0ca407
Ayan Masood <ayan.m.dev@gmail.com>
2023-05-07 23:47:33 +0530

    Update index.js

--- 13 - Advanced Javascript and DOM Manipulation/index.js
+++ 13 - Advanced Javascript and DOM Manipulation/index.js
@@ -1,5 +1,6 @@
 //Adding event listeners to all  the buttons with the class name 'drum' and putting it through a for loop so that it goes every button with class 'drum'   
 
+//Detecting Button Press
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
@@ -7,52 +8,60 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() { 
 
         var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
 
-        switch (buttonInnerHTML) {
-            
-            case "w":
-                function Audio (filelocation){
-                    var audio = new Audio(filelocation);
-                    audio.play();
-                    }
-                    Audio('sounds/tom-1.mp3');
-            break;
+    });
+}
 
+//Detecting Keyboard Press
+document.addEventListener("keydown", function(event) {
 
-            case "a":
-                var tom2 = new Audio ('sounds/tom-2.mp3');
-                tom2.play();
-            break;
-
-            case "s":
-                var tom3 = new Audio ('sounds/tom-3.mp3');
-                tom3.play();
-            break;
-
-            case "d":
-                var tom4 = new Audio ('sounds/tom-4.mp3');
-                tom4.play();
-            break;
-
-            case "j":
-                var snare = new Audio ('sounds/snare.mp3');
-                snare.play();
-            break;
-
-            case "k":
-                var bass = new Audio ('sounds/kick-bass.mp3');
-                bass.play();
-            break;
-
-            case "l":
-                var crash = new Audio ('sounds/crash.mp3');
-                crash.play();
-            break;
+    makeSound(event.key)
+});
 
 
-        default: console.log(buttonInnerHtml);
+function makeSound(key) {
+    
+    switch (key) {
+
+        case "w":
+            var tom1 = new Audio ('sounds/tom-1.mp3');
+            tom1.play();
+        break;
+
+        case "a":
+            var tom2 = new Audio ('sounds/tom-2.mp3');
+            tom2.play();
+        break;
+
+        case "s":
+            var tom3 = new Audio ('sounds/tom-3.mp3');
+            tom3.play();
+        break;
+
+        case "d":
+            var tom4 = new Audio ('sounds/tom-4.mp3');
+            tom4.play();
+        break;
+
+        case "j":
+            var snare = new Audio ('sounds/snare.mp3');
+            snare.play();
+        break;
+
+        case "k":
+            var bass = new Audio ('sounds/kick-bass.mp3');
+            bass.play();
+        break;
+
+        case "l":
+            var crash = new Audio ('sounds/crash.mp3');
+            crash.play();
+        break;
+
+
+        default: console.log(buttonInnerHTML);
         }
 
     });
-    
 }
